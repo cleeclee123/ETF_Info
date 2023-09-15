979003948a67f664de517dffac5167a8437ce0e1
Chris Lee <cl114@illinois.edu>
2023-09-14 22:49:44 -0500

    add flat vg object

--- vg.py
+++ vg.py
@@ -31,7 +31,7 @@ def filter_vg_fund_data(fund_data: dict) -> dict:
     filtered["vg_risk_code"] = risk.get("code", "DNE")
 
     def check_asset():
-        if profile.get("fundFact", {}).get("isBond", "DNE") == True: 
+        if profile.get("fundFact", {}).get("isBond", "DNE") == True:
             return "Fixed Income - Bond"
         if profile.get("fundFact", {}).get("isMoneyMarket", "DNE") == True:
             return "Fixed Income - Money Market"
@@ -41,95 +41,163 @@ def filter_vg_fund_data(fund_data: dict) -> dict:
             return "Fixed Income - Balanced"
         if profile.get("fundFact", {}).get("isStock", "DNE") == True:
             return "Equity"
+
     filtered["asset"] = check_asset()
 
     filtered["vol_date"] = risk.get("volatility", {}).get("asOfDate", "DNE")
-    filtered["vol_primary_benchmark"] = risk.get(
-        "volatility", {}).get("primaryBenchmarkName", "DNE")
-    filtered["vol_broadbase _benchmark"] = risk.get(
-        "volatility", {}).get("broadBasedBenchmarkName", "DNE")
-    filtered["vol_beta_primary"] = risk.get(
-        "volatility", {}).get("betaPrimary", "DNE")
-    filtered["vol_rSquared_primary"] = risk.get(
-        "volatility", {}).get("rSquaredPrimary", "DNE")
-    filtered["vol_beta_broadbased"] = risk.get(
-        "volatility", {}).get("betaBroadBased", "DNE")
-    filtered["vol_rSquared_broadbased"] = risk.get(
-        "volatility", {}).get("rSquaredBroadBased", "DNE")
+    filtered["vol_primary_benchmark"] = risk.get("volatility", {}).get(
+        "primaryBenchmarkName", "DNE"
+    )
+    filtered["vol_broadbase _benchmark"] = risk.get("volatility", {}).get(
+        "broadBasedBenchmarkName", "DNE"
+    )
+    filtered["vol_beta_primary"] = risk.get("volatility", {}).get("betaPrimary", "DNE")
+    filtered["vol_rSquared_primary"] = risk.get("volatility", {}).get(
+        "rSquaredPrimary", "DNE"
+    )
+    filtered["vol_beta_broadbased"] = risk.get("volatility", {}).get(
+        "betaBroadBased", "DNE"
+    )
+    filtered["vol_rSquared_broadbased"] = risk.get("volatility", {}).get(
+        "rSquaredBroadBased", "DNE"
+    )
 
     filtered["market_date"] = prices.get("market", {}).get("asOfDate", "DNE")
     filtered["market_price"] = prices.get("market", {}).get("price", "DNE")
-    filtered["market_price_change_amt"] = prices.get(
-        "market", {}).get("priceChangeAmount", "DNE")
-    filtered["market_price_change_pct"] = prices.get(
-        "market", {}).get("priceChangePct", "DNE")
+    filtered["market_price_change_amt"] = prices.get("market", {}).get(
+        "priceChangeAmount", "DNE"
+    )
+    filtered["market_price_change_pct"] = prices.get("market", {}).get(
+        "priceChangePct", "DNE"
+    )
 
     filtered["yield_date"] = yield_data.get("asOfDate", "DNE")
     filtered["yield_sec_pct"] = yield_data.get("yieldPct", "DNE")
 
     try:
-        filtered["yield_rating"] = yield_data.get(
-            "yieldNote", [])[0].get("footnoteCode", "DNE")
+        filtered["yield_rating"] = yield_data.get("yieldNote", [])[0].get(
+            "footnoteCode", "DNE"
+        )
     except:
         filtered["yield_rating"] = "DNE"
 
     filtered["ytd_date"] = ytd.get("asOfDate", "DNE")
-    filtered["ytd_reg_market_spread"] = "DNE" if ytd.get("marketPrice", "DNE") == "DNE" or ytd.get(
-        "regular", "DNE") == "DNE" else abs(int(float(ytd.get("regular", 0))) - int(float(ytd["marketPrice"])))
+    filtered["ytd_reg_market_spread"] = (
+        "DNE"
+        if ytd.get("marketPrice", "DNE") == "DNE" or ytd.get("regular", "DNE") == "DNE"
+        else abs(int(float(ytd.get("regular", 0))) - int(float(ytd["marketPrice"])))
+    )
 
     filtered["inception"] = performance.get("sinceInceptionAsOfDate", "DNE")
-    filtered["fund_performace_date"] = performance.get(
-        "fundReturn", {}).get("asOfDate", "DNE")
-    filtered["fund_ytd_return_pct"] = performance.get(
-        "fundReturn", {}).get("calendarYTDPct", "DNE")
-    filtered["fund_prev_month_return_pct"] = performance.get(
-        "fundReturn", {}).get("prevMonthPct", "DNE")
-    filtered["fund_three_month_return_pct"] = performance.get(
-        "fundReturn", {}).get("threeMonthPct", "DNE")
-    filtered["fund_one_year_return_pct"] = performance.get(
-        "fundReturn", {}).get("oneYrPct", "DNE")
-    filtered["fund_three_year_return_pct"] = performance.get(
-        "fundReturn", {}).get("threeYrPct", "DNE")
-    filtered["fund_five_year_return_pct"] = performance.get(
-        "fundReturn", {}).get("fiveYrPct", "DNE")
-    filtered["fund_ten_year_return_pct"] = performance.get(
-        "fundReturn", {}).get("tenYrPct", "DNE")
-    filtered["fund_lifetime_return_pct"] = performance.get(
-        "fundReturn", {}).get("sinceInceptionPct", "DNE")
+    filtered["fund_performace_date"] = performance.get("fundReturn", {}).get(
+        "asOfDate", "DNE"
+    )
+    filtered["fund_ytd_return_pct"] = performance.get("fundReturn", {}).get(
+        "calendarYTDPct", "DNE"
+    )
+    filtered["fund_prev_month_return_pct"] = performance.get("fundReturn", {}).get(
+        "prevMonthPct", "DNE"
+    )
+    filtered["fund_three_month_return_pct"] = performance.get("fundReturn", {}).get(
+        "threeMonthPct", "DNE"
+    )
+    filtered["fund_one_year_return_pct"] = performance.get("fundReturn", {}).get(
+        "oneYrPct", "DNE"
+    )
+    filtered["fund_three_year_return_pct"] = performance.get("fundReturn", {}).get(
+        "threeYrPct", "DNE"
+    )
+    filtered["fund_five_year_return_pct"] = performance.get("fundReturn", {}).get(
+        "fiveYrPct", "DNE"
+    )
+    filtered["fund_ten_year_return_pct"] = performance.get("fundReturn", {}).get(
+        "tenYrPct", "DNE"
+    )
+    filtered["fund_lifetime_return_pct"] = performance.get("fundReturn", {}).get(
+        "sinceInceptionPct", "DNE"
+    )
 
     filtered["market_price_performace_date"] = performance.get(
-        "marketPriceFundReturn", {}).get("asOfDate", "DNE")
+        "marketPriceFundReturn", {}
+    ).get("asOfDate", "DNE")
     filtered["market_price_ytd_return_pct"] = performance.get(
-        "marketPriceFundReturn", {}).get("calendarYTDPct", "DNE")
+        "marketPriceFundReturn", {}
+    ).get("calendarYTDPct", "DNE")
     filtered["market_price_prev_month_return_pct"] = performance.get(
-        "marketPriceFundReturn", {}).get("prevMonthPct", "DNE")
+        "marketPriceFundReturn", {}
+    ).get("prevMonthPct", "DNE")
     filtered["market_price_three_month_return_pct"] = performance.get(
-        "marketPriceFundReturn", {}).get("threeMonthPct", "DNE")
+        "marketPriceFundReturn", {}
+    ).get("threeMonthPct", "DNE")
     filtered["market_price_one_year_return_pct"] = performance.get(
-        "marketPriceFundReturn", {}).get("oneYrPct", "DNE")
+        "marketPriceFundReturn", {}
+    ).get("oneYrPct", "DNE")
     filtered["market_price_three_year_return_pct"] = performance.get(
-        "marketPriceFundReturn", {}).get("threeYrPct", "DNE")
+        "marketPriceFundReturn", {}
+    ).get("threeYrPct", "DNE")
     filtered["market_price_five_year_return_pct"] = performance.get(
-        "marketPriceFundReturn", {}).get("fiveYrPct", "DNE")
+        "marketPriceFundReturn", {}
+    ).get("fiveYrPct", "DNE")
     filtered["market_price_ten_year_return_pct"] = performance.get(
-        "marketPriceFundReturn", {}).get("tenYrPct", "DNE")
+        "marketPriceFundReturn", {}
+    ).get("tenYrPct", "DNE")
     filtered["market_price_lifetime_return_pct"] = performance.get(
-        "marketPriceFundReturn", {}).get("sinceInceptionPct", "DNE")
+        "marketPriceFundReturn", {}
+    ).get("sinceInceptionPct", "DNE")
 
     return filtered
 
 
-def create_vg_fund_info() -> pd.DataFrame:
+def flatten_json(y):
+    out = {}
+
+    def flatten(x, name=""):
+        if type(x) is dict:
+            for a in x:
+                flatten(x[a], name + a + "_")
+        elif type(x) is list:
+            i = 0
+            for a in x:
+                flatten(a, name + str(i) + "_")
+                i += 1
+        else:
+            out[name[:-1]] = x
+
+    flatten(y)
+    return out
+
+
+def create_vg_fund_info(parent_dir: str = None) -> pd.DataFrame:
     all_data = get_vg_fund_data()
 
     filtered = []
+    flatten = []
     for fund in all_data:
         filtered.append(filter_vg_fund_data(fund))
+        flatten.append(flatten_json(fund))
 
-    df = pd.DataFrame(filtered)
-    df.drop(df[df.ticker == "DNE"].index, inplace=True)
-    
-    curr_date = datetime.today().strftime('%Y-%m-%d')
-    df.to_excel(f"{curr_date}_vg_fund_info.xlsx", index=False, sheet_name=f"{curr_date}_vg_fund_info.xlsx")
+    curr_date = datetime.today().strftime("%Y-%m-%d")
+    wb_name = (
+        f"./{parent_dir}/{curr_date}_vg_fund_info.xlsx"
+        if (parent_dir)
+        else f"{curr_date}_vg_fund_info.xlsx"
+    )
 
-    return df
+    with pd.ExcelWriter(wb_name) as writer:
+        filtered_df = pd.DataFrame(filtered)
+        filtered_df.drop(filtered_df[filtered_df.ticker == "DNE"].index, inplace=True)
+        filtered_df.to_excel(writer, sheet_name="vg_fund_info_filtered", index=False)
+
+        flatten_df = pd.DataFrame(flatten)
+        flatten_df = flatten_df.fillna("DNE")
+        flatten_df.drop(
+            flatten_df[flatten_df.profile_ticker == "DNE"].index, inplace=True
+        )
+        flatten_df.to_excel(writer, sheet_name="vg_fund_info_flatten", index=False)
+
+    return filtered_df
+
+
+if __name__ == "__main__":
+    create_vg_fund_info("out")
+    # Input values in the function
